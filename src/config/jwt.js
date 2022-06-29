1d5f9c18cd99a809964bea28631037392fca78b8
Gaspor <gaspor991@gmail.com>
2022-06-29 17:20:13 -0300

    add: returning id

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -2,8 +2,8 @@ const jwt = require("jsonwebtoken");
 
 function jwtTokens({ id, username, email, cargo }) {
     const user = { id, username, email, cargo };
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20s' });
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '20m' });
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '200m' });
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '200m' });
     return ({ accessToken, refreshToken });
 
 }

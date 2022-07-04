74f2b71b2946850e51ef2a84c2b5a3c4d80c5de8
Gaspor <gaspor991@gmail.com>
2022-07-04 19:05:18 -0300

    add: user info in register

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 function jwtTokens({ id, username, email, cargo }) {
     const user = { id, username, email, cargo };
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '200m' });
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '10m' });
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1000m' });
     return ({ accessToken, refreshToken });
 
 }
